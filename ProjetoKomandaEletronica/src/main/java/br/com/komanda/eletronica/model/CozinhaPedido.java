398206395d75438affc8b4f5c5766872fd692156
mateus botelho <70180230+mateusbotelho12@users.noreply.github.com>
2021-04-08 20:18:41 -0300

    Criacao dos atributos do funcionario

--- ProjetoKomandaEletronica/src/main/java/br/com/komanda/eletronica/model/CozinhaPedido.java
+++ ProjetoKomandaEletronica/src/main/java/br/com/komanda/eletronica/model/CozinhaPedido.java
@@ -1,7 +1,5 @@
 package br.com.komanda.eletronica.model;
 
 public class CozinhaPedido {
-	String teste;
-	String teste2;
-
+	
 }
--- ProjetoKomandaEletronica/src/main/java/br/com/komanda/eletronica/model/Funcionario.java
+++ ProjetoKomandaEletronica/src/main/java/br/com/komanda/eletronica/model/Funcionario.java
@@ -1,5 +1,14 @@
 package br.com.komanda.eletronica.model;
 
 public class Funcionario {
+	
+	private int idFuncionario;
+	private int NumeroRegistro;
+	private boolean IsGerente;
+	private boolean IsExcluído ;
+	
+	Pessoa  pessoa;
+	
+	
 
 }
