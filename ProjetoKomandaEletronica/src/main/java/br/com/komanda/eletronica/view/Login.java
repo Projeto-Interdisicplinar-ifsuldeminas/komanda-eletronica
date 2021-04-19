eef8313cbd94176aea1868dec323e3550b3def7a
daniloliparini83 <80133292+daniloliparini83@users.noreply.github.com>
2021-04-19 10:03:58 -0300

    Criação de Splash e Login

---
+++ ProjetoKomandaEletronica/src/main/java/br/com/komanda/eletronica/view/Login.java
@@ -0,0 +1,111 @@
+package br.com.komanda.eletronica.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Dimension;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+@SuppressWarnings({ "serial", "unused" })
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	/*public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		
+		setSize(new Dimension(13, 0));
+		setResizable(false);
+		setUndecorated(true);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setMaximumSize(new Dimension(32739, 25000));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Login");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 25, 420, 27);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 90, 420, 33);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(10, 134, 420, 33);
+		panel.add(passwordField);
+		
+		JButton btnOk = new JButton("Login");
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					ValidaUsuario();			
+			}
+		});
+		btnOk.setBackground(new Color(0, 128, 0));
+		btnOk.setBounds(35, 222, 180, 23);
+		panel.add(btnOk);
+		
+		JButton btnCancel = new JButton("Cancelar");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnCancel.setBackground(new Color(255, 0, 0));
+		btnCancel.setBounds(225, 222, 180, 23);
+		panel.add(btnCancel);
+		
+		
+	}
+	
+	public void ValidaUsuario() {
+		this.dispose();
+		MainDashboard window = new MainDashboard();
+		window.GetFrame().setVisible(true);
+		window.GetFrame().setLocationRelativeTo(null);
+	}
+	
+	public void FechaSplashScreen(SplashScreen splash) {
+		splash.dispose();
+	}
+}
---
+++ ProjetoKomandaEletronica/src/main/java/br/com/komanda/eletronica/view/SplashScreen.java
@@ -0,0 +1,157 @@
+package br.com.komanda.eletronica.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import java.awt.CardLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.sql.SQLException;
+
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+
+@SuppressWarnings("unused")
+public class SplashScreen extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private static SplashScreen aux = null;
+	
+	//private final int duration;
+
+	
+
+	/**
+	 * Create the frame.
+	 */
+	
+	
+	
+	public SplashScreen() throws SQLException{
+		setLocationByPlatform(true);
+		
+		//duration = d;
+		
+		setResizable(false);
+		setUndecorated(true);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);		
+		setBounds(100, 100, 577, 443);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Inform\u00E1tica\\Documents\\Projetos Eclipse\\Komanda Eletr\u00F4nica\\komanda-eletronica\\ProjetoKomandaEletronica\\src\\main\\java\\img\\logo_if_resize.png"));
+		lblNewLabel.setBounds(10, 48, 547, 82);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("PROJETO INTERDISCIPLINAR");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_1.setBounds(10, 11, 547, 46);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Komanda Eletr\u00F4nica");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 28));
+		lblNewLabel_2.setBounds(10, 158, 547, 91);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Danilo Liparini Moraes - RA: 201924410096 ");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3.setBounds(0, 358, 567, 14);
+		panel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("Luciano Luiz Romero - RA: 201924410106");
+		lblNewLabel_3_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_3_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3_1.setBounds(0, 383, 567, 14);
+		panel.add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_3_2 = new JLabel("Mateus Botelho Ribeiro - RA: 201924410271");
+		lblNewLabel_3_2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_3_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3_2.setBounds(0, 408, 567, 14);
+		panel.add(lblNewLabel_3_2);
+		
+	}
+	
+	public void ShowLogin(SplashScreen splash) {
+		Login login = new Login();
+		login.setLocationRelativeTo(null);
+		//splash.dispose();
+		try {
+			Thread.sleep(5000);
+			//login.FechaSplashScreen(splash);
+			login.setVisible(true);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+								
+	}
+	
+	public void ShowSplash() {
+		try {
+			Thread.sleep(5000);
+			//login.FechaSplashScreen(splash);
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Login login = new Login();
+		login.setLocationRelativeTo(null);
+		//splash.dispose();
+		login.setVisible(true);			
+	}
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SplashScreen frame = new SplashScreen();
+					frame.setLocationRelativeTo(null);
+					frame.setVisible(true);
+					aux = frame;
+					//frame.ShowLogin(frame);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}		
+			}
+		});
+		try {
+			Thread.sleep(5000);
+			Login login = new Login();
+			login.setLocationRelativeTo(null);
+			//splash.dispose();
+			login.FechaSplashScreen(aux);
+			login.setVisible(true);			
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
